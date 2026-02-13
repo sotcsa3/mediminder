f32a00773dc0a6a50602762316438d66fbf49db5
Csaba Soti <csaba.soti@zetoinc.com>
2026-02-13 07:50:17 +0100

    chore: bump version to v1.3.3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
    MediMinder – Application Logic
    ============================================ */
 
-const APP_VERSION = '1.3.2';
+const APP_VERSION = '1.3.3';
 
 // NOTE: DB object is now defined in firebase-db.js
 
